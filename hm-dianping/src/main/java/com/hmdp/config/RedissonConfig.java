650721c3f8209a2be6a77fd4d17567076e87a02d
zhuojie <3382556398@qq.com>
2026-09-19 17:45:57 +0800

    Redisson错误修改

--- hm-dianping/src/main/java/com/hmdp/config/RedissonConfig.java
+++ hm-dianping/src/main/java/com/hmdp/config/RedissonConfig.java
@@ -11,7 +11,7 @@ public class RedissonConfig {
     @Bean
     public RedissonClient redissonClient(){
         Config config=new Config();
-        config.useSingleServer().setAddress("redis:localhost");
+        config.useSingleServer().setAddress("redis://localhost:6379");
         return Redisson.create(config);
     }
 }
